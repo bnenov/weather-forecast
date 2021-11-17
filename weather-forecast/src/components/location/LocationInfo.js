8ba818b89e54cd5360473be9ef5f888561aedc84
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-17 09:37:11 +0200

    Removed redundant div

--- weather-forecast/src/components/location/LocationInfo.js
+++ weather-forecast/src/components/location/LocationInfo.js
@@ -3,25 +3,23 @@ import classes from './LocationInfo.module.css';
 const LocationInfo = (props) => {
     return (
         <div className={classes.container}>
-            <div>
-                <div className={classes.location}>
-                    <h2>Weather in Sofia</h2>
-                    <p>600м надм</p>
+            <div className={classes.location}>
+                <h2>Weather in Sofia</h2>
+                <p>600м надм</p>
+            </div>
+            <div className={classes['time-and-weather']}>
+                <div className={classes.degrees}>
+                    <h2>10 °C</h2>
                 </div>
-                <div className={classes['time-and-weather']}>
-                    <div className={classes.degrees}>
-                        <h2>10 °C</h2>
+                <div className={classes.datetime}>
+                    <div>
+                        <p>8:36</p>
                     </div>
-                    <div className={classes.datetime}>
-                        <div>
-                            <p>8:36</p>
-                        </div>
-                        <div>
-                            <p>|</p>
-                        </div>
-                        <div>
-                            <p>17.11.2021</p>
-                        </div>
+                    <div>
+                        <p>|</p>
+                    </div>
+                    <div>
+                        <p>17.11.2021</p>
                     </div>
                 </div>
             </div>
