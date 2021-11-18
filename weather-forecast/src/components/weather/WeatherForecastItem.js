a75d3b1156e12fb5d468b749eba06ef1f3a0a4f7
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-18 17:12:03 +0200

    Added sample click handler function for the weather items. Added color change when hovering the search button.

--- weather-forecast/src/components/weather/WeatherForecastItem.js
+++ weather-forecast/src/components/weather/WeatherForecastItem.js
@@ -1,8 +1,14 @@
 import classes from './WeatherForecastItem.module.css';
 
 const WeatherForecastItem = (props) => {
+
+
+    const clickHandler = () => {
+        console.log("clicked me");
+    }
+
     return (
-        <li className={classes.day}>
+        <li className={classes.day} onClick={clickHandler}>
             <div>
                 <h3>{props.name}</h3>
                 <img src={props.icon} />
