a42ccc8aa9567a604f71a96988a4c6b6a872d7fa
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-18 17:40:02 +0200

    Added current date as part of the weather forecast list items

--- weather-forecast/src/components/weather/WeatherForecastItemList.js
+++ weather-forecast/src/components/weather/WeatherForecastItemList.js
@@ -2,11 +2,13 @@ import WeatherForecastItem from "./WeatherForecastItem";
 import classes from './WeatherForecastItemList.module.css';
 
 const WeatherForecastItemList = (props) => {
+
+    console.log(props);
     return (
         <ul className={classes.container}>
             {props.items.map(item => {
                 return (
-                    <WeatherForecastItem name={item.name} icon={item.icon} temperature={item.temperature} />
+                    <WeatherForecastItem id={item.id} name={item.name} icon={item.icon} temperature={item.temperature} currDate={item.currDate} />
                 )
             })}
         </ul>
