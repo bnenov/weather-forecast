a9ec32a863d9c15ac05681c574c9eb0dcdaf619b
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-19 11:37:43 +0200

    Added additional weather info section and made the list items clickable

--- weather-forecast/src/components/weather/WeatherForecast.js
+++ weather-forecast/src/components/weather/WeatherForecast.js
@@ -1,11 +1,20 @@
+import { useState } from "react"
 import WeatherForecastItemList from "./WeatherForecastItemList"
-
+import WeatherItemDetails from "./WeatherItemDetails"
 
 
 const WeatherForecast = (props) => {
+
+    const [itemClicked, setItemClicked] = useState(false);
+
+    const itemClickHandler = (isClicked) => {
+        setItemClicked(isClicked);
+    }
+
     return (
         <div>
-            <WeatherForecastItemList items={props.items} />
+            <WeatherForecastItemList items={props.items} onClick={itemClickHandler} />
+            {itemClicked && <WeatherItemDetails />}
         </div>
     )
 }
--- weather-forecast/src/components/weather/WeatherForecastItem.js
+++ weather-forecast/src/components/weather/WeatherForecastItem.js
@@ -1,10 +1,29 @@
+import { useEffect, useState } from 'react';
 import classes from './WeatherForecastItem.module.css';
 
 const WeatherForecastItem = (props) => {
 
+    const [isClicked, setIsClicked] = useState(false);
+
+    useEffect(() => {
+        props.onClick(isClicked)
+    }, [isClicked]);
+
+
+    const onClickHandler = () => {
+        if(isClicked) {
+            setIsClicked(false);
+        } else {
+            setIsClicked(true);
+        }
+        console.log("clicked");
+    }
+
+    const liClasses = `${classes.day} ${isClicked ? classes.clicked : ''}`;
+
 
     return (
-        <li key={props.id} className={classes.day}>
+        <li key={props.id} className={liClasses} onClick={onClickHandler}>
             <div>
                 <h3>{props.name}</h3>
                 <p>{props.currDate}</p>
