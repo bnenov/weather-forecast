1b6e805aeef4c171df303103dfad86118cd5957b
Borislav Nenov <borislav.nenov.93@gmail.com>
2021-11-30 09:43:36 +0200

    Added a constant for the API key

--- weather-forecast/src/utils/getCurrentWeatherData.js
+++ weather-forecast/src/utils/getCurrentWeatherData.js
@@ -1,7 +1,9 @@
+import { API_KEY } from "./Constants";
+
 export const getCurrentWeatherData = async (locationData) => {
     console.log('Location', locationData.city);
 
-    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${locationData.city},${locationData.country}&appid=31dc61f6c5690f83676fb79b56e362b0&units=metric`;
+    const urlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${locationData.city},${locationData.country}&appid=${API_KEY}&units=metric`;
 
     console.log(urlWeather);
     const response = await fetch(urlWeather);
